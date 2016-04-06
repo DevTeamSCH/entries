c3be10852c60991b2b0df389df5a8744f2e54c42
bence92 <bence92@sch.bme.hu>
2016-04-06 20:49:19 +0200

    logout redirect

--- entries/urls.py
+++ entries/urls.py
@@ -5,6 +5,6 @@ from pityu.urls import url_patterns
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
     url(r'^', include(url_patterns)),
 ]
